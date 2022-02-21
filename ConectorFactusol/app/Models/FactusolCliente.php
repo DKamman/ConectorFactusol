9eef66f42ef402f3c722afc106aaf3b0a47351b3
DKamman <42833740+DKamman@users.noreply.github.com>
2022-02-21 11:18:27 +0100

    Update Model naming convention

---
+++ ConectorFactusol/app/Models/FactusolCliente.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FactusolCliente extends Model
+{
+    use HasFactory;
+
+    public function getBearerToken() {
+        "codigoFabricante":"305",
+        "codigoCliente":"99973",
+        "baseDatosCliente":"FS305",
+        "password":"RExVUDczN2NkanZP"
+    }
+}
--- ConectorFactusol/app/Models/Producto.php
+++ ConectorFactusol/app/Models/Producto.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Http;
 
-class Productos extends Model
+class Producto extends Model
 {
     use HasFactory;
 
