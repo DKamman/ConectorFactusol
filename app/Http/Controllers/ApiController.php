2cd6ffcc67803593c302a9387bdb7c9c2f395628
DKamman <42833740+DKamman@users.noreply.github.com>
2022-03-14 16:31:07 +0100

    Create Pedido Filter

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -10,9 +10,12 @@ use App\Models\Pedido;
 use App\Models\FactusolApi;
 use App\Models\FactusolCliente;
 use App\Models\FactusolProducto;
+use App\Models\FactusolPedido;
+use Carbon\Carbon;
 
 class ApiController extends Controller
 {   
+    protected $dateParam = '2022-03-09';
     protected $apikey = '3741b78df9262be12be380987d275c6f';
 
     //Gets all clients from the 20Bananas API and renders them in a view
@@ -26,9 +29,7 @@ class ApiController extends Controller
 
     //Gets all orders from the 20Bananas API and renders them in a view
     public function getVbPedidos() {
-        // $dateParam = "2022-02-08";
-        $dateParam = '';
-        $response = Pedido::get($this->apikey, $dateParam);
+        $response = Pedido::get($this->apikey, $this->dateParam);
         $factusolData = Pedido::filter($response);
 
         return view('20bananas.pedidos', [
@@ -60,7 +61,7 @@ class ApiController extends Controller
     public function getFactusolProductos() {
         $token = FactusolApi::getBearerToken();
         $response = FactusolProducto::get($token);
-        $filtered = FactusolProducto::filter($response);
+        $filtered = FactusolProducto::filter($response, $token);
 
         return view('factusol.productos', [
             'response' => $filtered,
@@ -69,11 +70,8 @@ class ApiController extends Controller
 
     //Gets all clients from the Factusol API and posts them to the 20Bananas database
     public function postVbClientes() {
-        // $apikey = '3741b78df9262be12be380987d275c6f';
-        // $apikey = '';
         $token = FactusolApi::getBearerToken();
         $body = FactusolCliente::get($token);
-        // $body = '';
 
         $response = Cliente::post($this->apikey, $body);
 
@@ -100,7 +98,18 @@ class ApiController extends Controller
     }
 
     public function postFactusolPedidos() {
-        $response = Pedido::get($this->apikey, $dateParam);
+        $token = FactusolApi::getBearerToken();
+        $response = Pedido::get($this->apikey, $this->dateParam);
+        $filtered = FactusolPedido::filter($response);
+        $body = [
+            [
+                "columna"=>"CODPLC",
+                "dato"=>"11111"
+            ]
+        ];
+
+        $posted = FactusolPedido::post($token, $body);
+        dd($posted);
     }
 
     //Renders the POST clientes view to 20 Bananas
---
+++ app/Models/FactusolPedido.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Http;
+
+class FactusolPedido extends Model
+{
+    use HasFactory;
+    protected static $url = "https://api.sdelsol.com/admin/EscribirRegistro";
+
+    public function post($token, $body) {
+        {
+            $response = Http::withOptions([
+                'verify' => false,
+            ])->withToken($token)->
+            withBody(
+                `
+                    "ejercicio":"2022",
+                    "tabla":"F_PCL",
+                `, 'application/json'
+            )->post(FactusolPedido::$url, $body);
+    
+            dd($response['respuesta']);
+        }
+    }
+
+    public function filter($response) {
+        $columnKeys =[ 
+            'idpedido', 
+            'desdedispositivo', 
+            'codcliente', 
+            'nombrecliente', 
+            'fecha', 
+            'hora', 
+            'envioemail', 
+            'totalimporte', 
+            'enviado10', 
+            'servido10', 
+            'integradoERP10',
+            'codcomercial', 
+            'clienteParticular', 
+            'comentarios', 
+            'fechaEntrega', 
+            'numpedidoCliente', 
+            'rutaReparto', 
+            'enviadoPorComercial',
+            'productos'
+        ];
+
+        $orderArray = array();
+        $order = array();
+        $orderColumna = array();
+        $i = 0;
+
+        foreach ($response as $records) {
+            foreach ($records as $record) {
+                $orderColumna['columna'] = $columnKeys[$i];
+                $orderColumna['dato'] = $record;
+                array_push($order, $orderColumna);
+                $i++;
+            }
+            array_push($orderArray, $order);
+            $i = 0;
+            $order = array();
+        }
+
+        return $orderArray;
+    }
+}
