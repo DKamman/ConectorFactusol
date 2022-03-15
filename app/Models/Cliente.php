4d3f464dcc2f83747f482658647b29118c7b9fee
DKamman <42833740+DKamman@users.noreply.github.com>
2022-03-15 12:15:27 +0100

    Update Cliente.php

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -32,7 +32,6 @@ class Cliente extends Model
         ])->get(Cliente::$url);
 
         if ($response['response'] == 'ERROR') {
-            dd($response);
             return 'Unauthorized';
         }
 
