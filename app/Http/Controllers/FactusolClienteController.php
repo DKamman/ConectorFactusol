213a4d549c7f8ff14630615e43fc956767e1edc9
DKamman <42833740+DKamman@users.noreply.github.com>
2022-04-06 15:48:21 +0200

    Create FactusolClienteController.php

---
+++ app/Http/Controllers/FactusolClienteController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FactusolClienteController extends Controller
+{
+    public function index() {
+        //
+    }
+
+    public function get() {
+        //
+    }
+
+    public function post() {
+        //
+    }
+}
