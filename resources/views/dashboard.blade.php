f7ecb27536909039331a3773329d7b97be3cfdbe
DKamman <42833740+DKamman@users.noreply.github.com>
2022-04-25 13:53:07 +0200

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,6 +17,7 @@
                                 <a class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition" href="{{ route('20bananas.clientes.index') }}">Clientes</a>
                                 <a class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition" href="{{ route('20bananas.productos.index') }}">Productos</a>
                                 <a class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition" href="{{ route('20bananas.pedidos.index') }}">Pedidos</a>
+                                <a class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition" href="{{ route('20bananas.ofertas.index') }}">Ofertas</a>
                             </div>
                         </div>
                         <div class="column ml-10">
@@ -25,6 +26,8 @@
                             <div class="content flex flex-col">
                                 <a class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition" href="{{ route('factusol.clientes.index') }}">Clientes</a>
                                 <a class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition" href="{{ route('factusol.productos.index') }}">Productos</a>
+                                <a class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition" href="{{ route('factusol.pedidos.index') }}">Pedidos</a>
+                                <a class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition" href="{{ route('factusol.ofertas.index') }}">Ofertas</a>
                             </div>
                         </div>
                     </div>
